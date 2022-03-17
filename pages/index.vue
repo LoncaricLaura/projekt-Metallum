25dcf40d1d7e7cab182536bbe46f2a0e1c834895
LoncaricLaura <laura.loncaric032@gmail.com>
2022-03-17 17:38:37 +0100

    modified index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,133 @@
 <template>
 	<div>
 		<div class="bg u-flex u-flex-ai--c center">
-			<p class="title txt-align-c">Postavite granice <br> Ostavite vrata</p>
+			<p class="title txt-align-c">
+				Postavite granice <br />
+				Ostavite vrata
+			</p>
 		</div>
-		<div class="group u-flex u-flex-fd--c hor-center txt-align-c">
+		<div class="group u-flex u-flex-fd--c hor-center txt-align-c ptb-main">
 			<h2 class="mb-md">Ograde i ogradni sustav</h2>
-			<p class="text ">Nije bitno samo ograditi radni prostor ili prostor sačuvati od upada nezvanih gostiju, već se ograda mora uklapati u okolinu i činiti je privlačnijom. To je moguće sa odabirom naših ograda koje su čvrste, trajne i privlačne za oko. Odaberite ogradu koja naglašava Vaš stil!</p>
+			<p class="text">
+				Nije bitno samo ograditi radni prostor ili prostor sačuvati od upada nezvanih gostiju, već se ograda mora uklapati u okolinu i činiti je privlačnijom. To je
+				moguće sa odabirom naših ograda koje su čvrste, trajne i privlačne za oko. Odaberite ogradu koja naglašava Vaš stil!
+			</p>
 		</div>
 
-		<div class="u-flex hor-center plr-9xl " id="pletivo">
-				<div class="u-flex-fd--c center">
-					<h2>PLETIVO</h2>
+		<!-- Pletivo -->
+		<div class="box u-flex u-flex-fd--c mlr-main">
+			<div class="box-item u-flex u-flex-ai--c">
+				<div class="ml-boxWrap box-item--text">
+					<h2>Pletivo</h2>
 					<div class="btn mt-5xl">SAZNAJTE VIŠE</div>
 				</div>
+				<div class="pos-a box-item--image u-flex u-flex-jc--fe">
+					<img src="/img/fence_blue_1.png" />
+				</div>
+			</div>
+
+			<!-- Valovita mreza -->
+			<div class="u-grid u-grid-2 mt-lg">
+				<div class="box-item u-flex u-flex-ai--c">
+					<div class="ml-boxWrap box-item--text">
+						<h2>Valovita (vibro) mreža</h2>
+						<div class="btn mt-5xl">SAZNAJTE VIŠE</div>
+					</div>
+					<div class="pos-a box-item--image u-flex u-flex-jc--fe">
+						<img src="/img/Fence-Knits.png" />
+					</div>
+				</div>
+
+				<!-- Vrata -->
+				<div class="box-item u-flex u-flex-ai--c">
+					<div class="ml-boxWrap box-item--text">
+						<h2>Vrata</h2>
+						<div class="btn mt-5xl">SAZNAJTE VIŠE</div>
+					</div>
+					<div class="pos-a box-item--image u-flex u-flex-jc--fe">
+						<img src="/img/Garden-Door.J03.png" />
+					</div>
+				</div>
+			</div>
+
+			<!-- Panelni sustav -->
+			<div class="box-item u-flex u-flex-ai--c mt-lg">
+				<div class="ml-boxWrap box-item--text">
+					<h2>Panelni sustav</h2>
+					<div class="btn mt-5xl">SAZNAJTE VIŠE</div>
+				</div>
+				<div class="pos-a box-item--image u-flex u-flex-jc--fe">
+					<img src="/img/Barbed-Wire-Fence.G03.png" />
+				</div>
+			</div>
+
+			<!-- Pruzni sustav -->
+			<div class="box-item u-flex u-flex-ai--c mtb-lg">
+				<div class="ml-boxWrap box-item--text">
+					<h2>Pružni sustav</h2>
+					<div class="btn mt-5xl">SAZNAJTE VIŠE</div>
+				</div>
+				<div class="pos-a box-item--image u-flex u-flex-jc--fe">
+					<img src="/img/fence_blue_1.png" />
+				</div>
+			</div>
+		</div>
+
+		<div class="group u-flex hor-center txt-align-c">
+			<p class="text mtb-xl">
+				Naši proizvodi koriste se u izgradnji nosiva konstrukcija te u svim vrstama ograda. Kako u industrijskim tako i u privatnim sektorima.
+			</p>
+		</div>
+
+		<div class="center">
+			<h2 class="mtb-6xl">Usluge</h2>
+		</div>
+
+		<div class="box-services u-grid u-grid-4 plr-box box-services--text txt-align-c">
+			<div>
+				<img src="/img/field.png" class="mb-lg"/>
+				<p>Izvid terena</p>
+			</div>
+			<div>
+				<img src="/img/docs.png" class="mb-lg" />
+				<p>Izrada nacrtne dokumentacije za ograde dizajna po želji klijenta</p>
+			</div>
+				<div>
+				<img src="/img/fence-door.png" class="mb-4xl"/>
+				<p>Proizvodnja ograda i vrata</p>
+			</div>
+			<div>
+				<img src="/img/protection-el.png" class="mb-lg"/>
+				<p>Antikorozivna zaštita elemenata</p>
+			</div>
+		</div>
+
+		<div class="box-services u-grid u-grid-4 plr-box mtb-box box-services--text txt-align-c">
+			<div>
+				<img src="/img/drilling.png" class="mb-lg"/>
+				<p>Dijamantno bušenje armiranog betona</p>
+			</div>
+			<div>
+				<img src="/img/installation.png" class="mb-lg" />
+				<p>Montaža ograde i vratiju</p>
+			</div>
+				<div>
+				<img src="/img/automation.png" class="mb-lg"/>
+				<p>Automatizacija vratiju</p>
+			</div>
+			<div>
+				<img src="/img/service.png" class="mb-lg"/>
+				<p>Servisiranje na terenu</p>
+			</div>
 		</div>
 	</div>
 </template>
 
-
-<script>
-
-</script>
+<script></script>
 
 <style lang="scss" scoped>
-
 .bg {
-	background: #428ACE;
+	background: #428ace;
 	background-image: url('/img/6.jpg');
 	background-blend-mode: multiply;
 	height: 944px;
@@ -41,15 +143,7 @@
 }
 
 .group {
-	padding-top: 150px;
-	padding-bottom: 150px;
 	width: 684px;
-}
-
-@media screen and (max-width: 640px) {
-	.group {
-		width: auto;
-	}
 }
 
 h2 {
@@ -67,33 +161,44 @@ h2 {
 	line-height: 32.4px;
 }
 
-#pletivo {
-	background: url('/img/fence_blue_1.png');
-	background-position: right;
-	background-repeat: no-repeat;
-	background-color: $metallum0;
+.box {
 	font-family: $ff-serif;
 	font-weight: $fw-normal;
-	width: 1400px;
-	height: 493px;
 }
 
-
-@media screen and (max-width: 768px) {
-	#pletivo {
-		flex-direction: column;
+.box-item {
+	background-color: $metallum0;
+	width: 100%;
+	height: 493px;
+	position: relative;
+	&--image {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		img {
+			object-fit: cover;
+		}
 	}
 }
 
 .btn {
-  background-color: $coral;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 14px;
+	background-color: $coral;
+	border: none;
+	color: white;
+	padding: 15px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 14px;
 }
 
+.box-services {
+	width: 100%;
+	&--text {
+		font-family: $ff-serif;
+		color: $iron80;
+		font-size: 27px;
+		line-height: 160%;
+	}
+}
 </style>
